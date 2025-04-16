a120cfbfe2b44ea265a21739cf89a45fa680e07f
Mamuun <almamun19403.com>
2025-04-16 23:44:44 +0600

    Added folders with correct casing

--- HomeApp/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-class HomeAppConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'HomeAPP'
-
-    def ready(self):
-        import HomeAPP.signals 
-        
